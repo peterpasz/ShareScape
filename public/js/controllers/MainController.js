b0019bebdd92efae7eccbc32a1dea1d95dafb68a
peterpasz <peterpasz@hotmail.com>
2017-03-13 18:07:01 -0400

    Fixing end points for heroku hosting and local testing

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,9 +1,11 @@
+var endpoint = "http://localhost:3000/api/posts" || "https://sharescape.herokuapp.com";
+
 //Angular JS
 angular.module("myApp", [])
 	.controller("MainController", ["$scope", "$http", function($scope, $http) {
 		$scope.message = "Hello World!",
 		
-		$http.get('https://sharescape.herokuapp.com/api/posts')
+		$http.get(endpoint)
 		.success(function (posts) {
 			console.log(posts)
 		  $scope.posts = posts
