0c35bac0c268f88c07fc1b21bf0ccb18c6cec5d2
peterpasz <peterpasz@hotmail.com>
2017-03-13 18:18:57 -0400

    Working ver

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -3,7 +3,14 @@ angular.module("myApp", [])
 	.controller("MainController", ["$scope", "$http", function($scope, $http) {
 		$scope.message = "Hello World!",
 		
-		$http.get('https://sharescape.herokuapp.com/api/posts'||'http://localhost:3000/api/posts')
+		/*
+		When deploying to heroku, change 
+		'http://localhost:3000/api/posts'
+		to
+		'https://sharescape.herokuapp.com/api/posts'
+		*/
+		
+		$http.get('http://localhost:3000/api/posts')
 		.success(function (posts) {
 			console.log(posts)
 		  $scope.posts = posts
